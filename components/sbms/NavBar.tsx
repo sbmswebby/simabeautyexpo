af1e554253bded5243d4bf0a310ef60f0d0f43c2
sbmswebby <sbmswebby@gmail.com>
2026-01-16 14:56:21 +0530

    updated nav bar

--- components/sbms/NavBar.tsx
+++ components/sbms/NavBar.tsx
@@ -9,12 +9,12 @@ const Navbar = () => {
         <Link href="/" className="logo">SIMA Beauty Expo</Link>
         <ul className="nav-links">
           
-          <li><a href="#about">About</a></li>
+          <li><a href="#about"><Link href="/">About</Link></a></li>
           <li><Link href="/bbn_directors">BBN Directors</Link></li>
           <li><Link href="/events">Events</Link></li>
           <li><Link href="/gallery">Gallery</Link></li>
-          <li><a href="#pageant">Pageant</a></li>
-          <li><a href="#contact">Contact</a></li>
+          <li><a href="#pageant"><Link href="/">Pageant</Link></a></li>
+          <li><a href="#contact"><Link href="/">Contact</Link></a></li>
           <li><Link href="/signup">Sign Up</Link></li>
           <li><Link href="/login">Login</Link></li>
         </ul>
